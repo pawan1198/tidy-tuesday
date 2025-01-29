8ed47313b7eb6659daa918759ceac4cc8f09194d
pawan1198 <pawanjangra1198@gmail.com>
2025-01-29 21:50:04 +0530

    update plot

--- 2025/2025-01-28/2025_01_28.R
+++ 2025/2025-01-28/2025_01_28.R
@@ -13,50 +13,49 @@ water_insecurity_2022 <- data$water_insecurity_2022
 water_insecurity_2023 <- data$water_insecurity_2023
 
 ## Combine data for easier analysis
-water_insecurity_combined <- bind_rows(
-  mutate(water_insecurity_2022, year = 2022),
-  mutate(water_insecurity_2023, year = 2023)
+combined <- bind_rows(
+    mutate(water_insecurity_2022, year = 2022),
+    mutate(water_insecurity_2023, year = 2023)
 )
 
 ## Explore the data
-summary(water_insecurity_combined)
-str(water_insecurity_combined)
+summary(combined)
+str(combined)
 
 ## Calculate the change in percent lacking plumbing between years
-water_insecurity_combined <- water_insecurity_combined %>%
-  group_by(name) %>%
-  mutate(change_percent = percent_lacking_plumbing - lag(percent_lacking_plumbing))
+combined <- combined %>%
+    group_by(name) %>%
+    mutate(change_percent = percent_lacking_plumbing - lag(percent_lacking_plumbing))
 
-# Create a scatter plot to visualize the change
-scatter_plot <- ggplot(water_insecurity_combined, aes(x = percent_lacking_plumbing, y = change_percent)) +
-  geom_point(alpha = 0.5) +
-  labs(
-    title = "Change in Percent of Households \nLacking Plumbing Facilities",
-    x = "Percent Lacking Plumbing",
-    y = "Change in Percent Lacking Plumbing (2023 - 2022)"
-  )+
-    theme_bw(base_size=10)
+## Create a scatter plot to visualize the change
+plot1<- ggplot(combined, aes(x = percent_lacking_plumbing, y = change_percent)) +
+    geom_point(alpha = 0.5) +
+    labs(
+        title = "Change in Percent of Households \nLacking Plumbing Facilities",
+        x = "Percent Lacking Plumbing",
+        y = "Change in Percent Lacking Plumbing (2023-2022)"
+    ) +
+    theme_bw(base_size = 10)
 
-scatter_plot
+plot1
 
-# Create a density plot to visualize the distribution of changes
-density_plot <- ggplot(water_insecurity_combined, aes(x = change_percent)) +
-  geom_density() +
-  labs(
-    title = "Distribution of Changes in \nPercent Lacking Plumbing",
-    x = "Change in Percent Lacking Plumbing (2023 - 2022)"
-  ) +
-    theme_bw(base_size=10)
+## Create a density plot to visualize the distribution of changes
+plot2 <- ggplot(combined, aes(x = change_percent)) +
+    geom_density() +
+    labs(
+        title = "Distribution of Changes in \nPercent Lacking Plumbing",
+        x = "Change in Percent Lacking Plumbing (2023-2022)"
+    ) +
+    theme_bw(base_size = 10)
 
-density_plot
+plot2
 
 
-# Combine plots using ggpubr
-combined_plot <- ggarrange(scatter_plot, density_plot, ncol = 2, common.legend = TRUE)
+## Combine plots using ggpubr
+combined_plot <- ggarrange(plot1, plot2, ncol = 2, common.legend = TRUE)
 
-# Print the combined plot
 combined_plot
 
-# Save the plot (optional)
+## Save the plot
 ggsave("water_insecurity_changes.png", plot = combined_plot, width = 8, height = 4)
 
